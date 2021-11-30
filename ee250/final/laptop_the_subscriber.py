4ae0efc49714f19c5c13cbd0f9a1b828dd03af37
Alena Novikova <anovikov@usc.edu>
2021-11-30 10:23:09 -0800

    path

--- ee250/final/laptop_the_subscriber.py
+++ ee250/final/laptop_the_subscriber.py
@@ -9,9 +9,9 @@ import sys
 import time
 # By appending the folder of all the GrovePi libraries to the system path here,
 # we are successfully `import grovepi`
-sys.path.append('../Software/Python/')
+sys.path.append('../../Software/Python/')
 # This append is to support importing the LCD library.
-sys.path.append('../Software/Python/grove_rgb_lcd')
+sys.path.append('../../Software/Python/grove_rgb_lcd')
 
 import grovepi
 
