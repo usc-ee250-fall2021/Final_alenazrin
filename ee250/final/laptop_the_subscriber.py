6b0c52630be9d88bc90451e7801d17cbf5de3385
Azrin Khan <ak_984@usc.edu>
2021-11-30 15:49:37 -0800

    Update laptop_the_subscriber.py

--- ee250/final/laptop_the_subscriber.py
+++ ee250/final/laptop_the_subscriber.py
@@ -13,9 +13,9 @@ import time
 # This append is to support importing the LCD library.
 #sys.path.append('../../Software/Python/grove_rgb_lcd')
 
-import grovepi
+#import grovepi
 
-from grove_rgb_lcd import *
+#from grove_rgb_lcd import *
 
 server_weather = 0
 
